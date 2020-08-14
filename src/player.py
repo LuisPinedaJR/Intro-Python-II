8df5027af1bac02b1970323e9c1acfbc9d1b0e92
Luis Pineda <lspnd00@gmail.com>
2020-08-13 22:22:35 -0400

    mvp

--- src/player.py
+++ src/player.py
@@ -1,5 +1,13 @@
-# Write a class to hold player information, e.g. what room they are in
-# currently.
 class Player:
-    def __init__(self, current_room=None):
+    def __init__(self, current_room, inventory):
         self.current_room = current_room
+        self.inventory = inventory
+    
+    def get_item(self, item):
+        self.inventory.append(item)
+        self.current_room.items.remove(item)
+
+    
+    def drop_item(self, item):
+        self.current_room.items.append(item)
+        self.inventory.remove(item)
--- src/tempCodeRunnerFile.py
+++ src/tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-         chamber! Sadly, it has already been completely emptied by
-                earlier adventurers. The only exit is to the south.""")
+e
